fa048abfbc7b4ae91c7736d6d1d2240ae59ae9cc
shnannan <s10223327@connect.np.edu.sg>
2023-05-06 19:35:12 +0800

    Updated Practical 2!

--- app/src/main/java/sg/edu/np/mad/week2project/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week2project/MainActivity.java
@@ -3,12 +3,31 @@ package sg.edu.np.mad.week2project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    final String title = "Main Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(title, "Create!");
+        // Get the User object
+        User myUser = new User();
+        // getting username and passcode
+        myUser.getName();
+        myUser.getDescription();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.v(title, "Start!"); // title, msg (logging) which of these functions am i in
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(title, "Resume");
     }
 }
